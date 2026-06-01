ae41c53319ab01e5dbec79c4ac57b79c5fe7c981
Sowmya Macherla <sowmyamacherla1519@gmail.com>
2026-06-01 16:09:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import hashlib
+
+filename = "monitored_file.txt"
+
+with open(filename, "rb") as file:
+    content = file.read()
+
+file_hash = hashlib.sha256(content).hexdigest()
+
 print("Cyber Sentinel Started")
-print("Security Monitoring System Online")
+print("File:", filename)
+print("SHA-256 Hash:", file_hash)
